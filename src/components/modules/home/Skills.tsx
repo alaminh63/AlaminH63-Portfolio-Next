0e1aacfa34dcb420f258fc48bfe9ad94eeeb5c36
MD Al Amin Hasan <alaminhasanpro@gmail.com>
2025-05-09 22:27:00 +0600

    fixed banner

--- src/components/modules/home/Skills.tsx
+++ src/components/modules/home/Skills.tsx
@@ -1,7 +1,6 @@
 import { skills } from '@/data/home-data';
-
+import Image from 'next/image';
 import React from 'react';
-
 
 export default function Skills() {
     return (
@@ -10,12 +9,23 @@ export default function Skills() {
             <h2 className="text-3xl font-bold mb-6">My Skill</h2>
             <div className="flex space-x-6 mb-6">
                 {skills.map((skill, index) => (
-                    <img
+                    <div 
                         key={index}
-                        src={skill.image}
-                        alt={skill.name}
-                        className="w-20 h-20"
-                    />
+                        className="group relative flex items-center justify-center border p-2 rounded-md" 
+                    >
+                        <Image
+                            src={skill.image}
+                            alt={skill.name}
+                            height={80}
+                            width={80}
+                            className="transition-transform duration-300 group-hover:scale-110 "
+                        />
+                        <div className="absolute  -bottom-10 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                            <span className="bg-black bg-opacity-70 text-white px-2 py-1 rounded text-sm">
+                                {skill.name}
+                            </span>
+                        </div>
+                    </div>
                 ))}
             </div>
             <button className="bg-red-500 text-white px-6 py-2 rounded-full hover:bg-red-600 transition-transform transform hover:scale-105">
